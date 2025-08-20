05384a9792b255dd16a187dfe443854ac3f1e44e
Xavier Geerinck <xavier.geerinck@gmail.com>
2025-08-20 17:37:58 +0200

    fix: move constant to main app for now
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- apps/sim/lib/sim-agent/constants.ts
+++ apps/sim/lib/sim-agent/constants.ts
@@ -1,1 +1,2 @@
-export const SIM_AGENT_API_URL_DEFAULT = 'https://agent.scrydon.eu'
+export const SIM_AGENT_API_URL_DEFAULT = 'https://demo.scrydon.com'
+// export const SIM_AGENT_API_URL_DEFAULT = 'https://agent.scrydon.com'
